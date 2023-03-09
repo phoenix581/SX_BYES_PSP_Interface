7c722573e9c23968180986f197c1a0aaf4144a8f
ja.jacoby <ja.jacoby@bouygues-es.com>
2023-03-09 05:09:27 +0000

    09.02.2023

--- module/src/main/java/com/byes/paap/rest/byespspinterface/WBSItemInterface.java
+++ module/src/main/java/com/byes/paap/rest/byespspinterface/WBSItemInterface.java
@@ -3,10 +3,12 @@ package com.byes.paap.rest.byespspinterface;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.byes.paap.rest.byespspinterface.dto.ResponseDTO;
 import com.byes.paap.rest.byespspinterface.dto.WBSItemsDTO;
 import com.planonsoftware.jaxrs.api.v9.context.IJaxRsResourceContext;
 
@@ -18,9 +20,13 @@ public class WBSItemInterface
 
     @POST
     @Path("/updateWBSItems")
+    @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createOrder(WBSItemsDTO WBSItems) {
-        return Response.ok().build();
+        ResponseDTO response = new ResponseDTO();
+        response.setStatus(200);
+        response.setStatusDescription("WBSItem created/updated.");
+        return Response.ok().entity(response).build();
     }
            
 }
